e02ee061112ffe5c1d47a2a8adde6dac5c55c24a
Korey Lo <koreylo9@gmail.com>
2020-10-16 12:35:35 -0700

    add travel request component

---
+++ src/app/travel-request/travel-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travel-request',
+  templateUrl: './travel-request.component.html',
+  styleUrls: ['./travel-request.component.css']
+})
+export class TravelRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
