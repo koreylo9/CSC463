111cecbcaf4b768b540c73d5341cad9002a55881
Korey Lo <koreylo9@gmail.com>
2020-10-16 16:45:52 -0700

    added form plus ngmodel for price

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TravelRequestComponent } from './travel-request/travel-request.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TravelRequestComponent } from './travel-request/travel-request.componen
     TravelRequestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/travel-request/travel-request.component.ts
+++ src/app/travel-request/travel-request.component.ts
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class TravelRequestComponent implements OnInit {
 
   constructor() { }
+  // tslint:disable-next-line: no-inferrable-types
+  passengers: number;
+  price: number;
+
+  // tslint:disable-next-line: typedef
+  public calculatePrice() {
+    if (isNaN(this.passengers)){
+        this.passengers = 0;
+    }
+    this.passengers = +this.passengers;
+    console.log(this.passengers);
+    this.price = 150 * this.passengers;
+}
 
   ngOnInit(): void {
   }
+
 
 }
