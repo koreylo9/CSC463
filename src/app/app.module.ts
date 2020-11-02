2f81e23808dc58c1c384141b76f426e9e596c2bf
Korey Lo <koreylo9@gmail.com>
2020-11-02 14:08:44 -0800

    added angular routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TravelRequestComponent } from './travel-request/travel-request.component';
 import { FormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  { path: 'travelRequest', component: TravelRequestComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
